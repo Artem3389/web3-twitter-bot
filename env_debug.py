07848d96ff072303632816263f710d17cd462215
Tas35 <111118839+Artem3389@users.noreply.github.com>
2025-05-14 11:25:30 +0200

    Update env_debug.py

--- env_debug.py
+++ env_debug.py
@@ -5,7 +5,7 @@ print("🔍 ALL ENVIRONMENT VARIABLES:")
 for key, value in os.environ.items():
     print(f"{key} = {value}")
 
-API_TOKEN = os.getenv("API_TOKEN")
+API_TOKEN = os.getenv("7670472776:AAFKugaQ4aBEM3lN9ioQJbRrIT3JI6zaM")
 print("🔍 DEBUG_API_TOKEN:", repr(API_TOKEN))
 
 if not API_TOKEN:
